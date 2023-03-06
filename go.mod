d8668b4fa09e922f97185a73b2b1fd591ac73d80
unknown <nicodumans@gmail.com>
2023-03-07 00:18:37 +0100

    Add timeout for ping

--- go.mod
+++ go.mod
@@ -5,12 +5,12 @@ go 1.20
 require (
 	github.com/hypolas/hypolashlckhelpers v0.0.10
 	github.com/hypolas/hypolashlckhttp v0.0.4
+	github.com/hypolas/hypolashlckping v0.0.3
 )
 
 require (
 	github.com/go-ping/ping v1.1.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	github.com/hypolas/hypolashlckping v0.0.1 // indirect
 	github.com/hypolas/hypolaslogger v0.0.10 // indirect
 	github.com/hypolas/readjsonfromflatpath v0.0.3 // indirect
 	golang.org/x/net v0.8.0 // indirect
--- healthcheck/buildmulti.sh
+++ healthcheck/buildmulti.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-GOOS=windows GOARCH=amd64 go build -trimpath -ldflags "-w -h -H windowsgui  -extldflags=-static" -o bin/healthcheck_hpl-$1-win-amd64.exe .
-GOOS=linux GOARCH=amd64 go build -trimpath -ldflags "-w -h -extldflags=-static" -o bin/healthcheck_hpl-$1-linux-amd64 .
-cp bin/healthcheck_hpl-$1-win-amd64.exe bin/healthcheck_hpl-$1-compressed-win-amd64.exe
-cp bin/healthcheck_hpl-$1-linux-amd64 bin/healthcheck_hpl-$1-compressed-linux-amd64
+GOOS=windows GOARCH=amd64 go build -trimpath -ldflags "-w -h -H windowsgui  -extldflags=-static" -o bin/healthcheck_hpl-win-amd64-$1.exe .
+GOOS=linux GOARCH=amd64 go build -trimpath -ldflags "-w -h -extldflags=-static" -o bin/healthcheck_hpl-linux-amd64-$1 .
+cp bin/healthcheck_hpl-win-amd64-$1.exe bin/healthcheck_hpl-win-amd64-$1-compressed.exe
+cp bin/healthcheck_hpl-linux-amd64-$1 bin/healthcheck_hpl-linux-amd64-$1-compressed
